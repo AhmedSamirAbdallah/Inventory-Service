bb59a6dd5a394d8b1fe5cdcc66b2657458678ff1
Ahmed Samir <ahmedsamir661998@gmail.com>
2024-09-08 21:31:38 +0300

    [start]

---
+++ src/main/java/com/service/inventory/ServiceInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.service.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/inventory/ServiceInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
